2a5fbd301446ffa4ecc20706d69594ff78fb2460
Alecia Vogel <hello@alecia.ca>
2023-01-10 09:53:37 -0700

    Add some tests that should test, just to test my github action

--- cypress/e2e/accessibility.cy.js
+++ cypress/e2e/accessibility.cy.js
@@ -1,10 +1,20 @@
 /// <reference types="Cypress" />
 
-describe("Accessibility tests", () => {
-  beforeEach(() => {
-    cy.visit("/blog").get("main").injectAxe();
+// describe("Accessibility tests", () => {
+//   beforeEach(() => {
+//     cy.visit("/blog").get("main").injectAxe();
+//   });
+//   it("Has no detectable accessibility violations on load", () => {
+//     cy.checkA11y();
+//   });
+// });
+
+describe("Checking that tests are passing", () => {
+  it("should visit", () => {
+    cy.visit("/");
   });
-  it("Has no detectable accessibility violations on load", () => {
-    cy.checkA11y();
+
+  it("Does not do much!", () => {
+    expect(true).to.equal(true);
   });
 });
