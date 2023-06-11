a45a89e9b5cc86d8d800ed88cbc1864a03d46e4c
Alecia Vogel <hello@alecia.ca>
2023-06-10 23:15:29 -0600

    Well it looks okay on my macbook...

--- src/components/pages/about/AboutHeader.tsx
+++ src/components/pages/about/AboutHeader.tsx
@@ -1,11 +1,44 @@
+"use client";
+import { useState, useEffect } from "react";
+import { useLocomotiveScroll } from "react-locomotive-scroll";
+
 import AleciaCouch from "@/components/vectors/AleciaCouchSvg";
 import RotatingText from "./RotatingText";
 
 export default function AboutHeader() {
+  const { scroll } = useLocomotiveScroll();
+  const [scrollPercent, setScrollPercent] = useState(0);
+
+  const scrollListener = (windowScrollTop: number) => {
+    const progress = windowScrollTop;
+    const percent = (progress / window.innerHeight) * 1.1;
+
+    if (percent <= 0) {
+      return setScrollPercent(0);
+    }
+    if (percent >= 100) {
+      return setScrollPercent(100);
+    }
+    setScrollPercent(percent);
+  };
+
+  useEffect(() => {
+    if (scroll) {
+      scroll.on("scroll", (event: any) => {
+        scrollListener(event.scroll.y);
+      });
+    }
+  }, [scroll]);
+
   return (
     <section className={`about-header`}>
-      <RotatingText className="spinning-text" />
-      <div className="w-full couch-svg" data-scroll data-scroll-speed="2">
+      <div
+        style={{ opacity: 1 - scrollPercent }}
+        className="transition-opacity duration-300 ease-out"
+      >
+        <RotatingText className="spinning-text" />
+      </div>
+      <div className="w-full couch-svg">
         <AleciaCouch />
       </div>
     </section>
--- src/components/pages/about/AboutMain.tsx
+++ src/components/pages/about/AboutMain.tsx
@@ -5,22 +5,10 @@ export default function AboutMain() {
     <div className="relative overflow-hidden pb-16 pt-48 mt-[-152px] z-40 leading-loose h-full">
       <div className="about-content">
         <div className="about-intro">
-          <h2>My happy place</h2>
+          <h2 className="font-serif">Hi, I'm Alecia.</h2>
           <p>
-            On the frontend, my favorite projects are built with <strong>ReactJS</strong> and{" "}
-            <strong>TypeScript</strong>. I've used a range of frameworks in the backend, but the
-            vast majority of my time has been spent working in <strong>Ruby on Rails</strong> and{" "}
-            <strong>Node</strong> environments. My portfolio website is built with{" "}
-            <strong>GatsbyJS</strong> and you can{" "}
-            <a href="https://github.com/aleciavogel/alecia.ca" target="_blank" rel="noreferrer">
-              view the source code on GitHub
-            </a>{" "}
-            for a taste of my development style.
-          </p>
-          <p>
-            While I've always been a very independent worker, I perform my best when there are
-            opportunities to collaborate with like-minded designers and developers. I thrive in
-            workplaces where I'm given the freedom to be creative, take risks, and experiment.
+            I'm a developer and designer with a passion for creating delightful, accessible, and
+            functional websites.
           </p>
         </div>
       </div>
