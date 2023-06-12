8ef786f27bbf469facb50d1759912170a55c25f2
Alecia Vogel <hello@alecia.ca>
2023-06-12 03:09:28 -0600

    Fix lint errors

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -8,7 +8,7 @@ export default function Providers({ children }: { children: React.ReactNode }) {
 
   useEffect(() => {
     setMounted(true);
-  });
+  }, [setMounted]);
 
   if (!mounted) {
     return null;
--- src/hooks/useTheme.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import { useContext } from "react";
-import { ThemeContext } from "@/contexts/ThemeContext";
-
-const useThemeContext = () => useContext(ThemeContext);
-
-export default useThemeContext;
