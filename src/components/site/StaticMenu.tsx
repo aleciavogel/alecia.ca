e0f0db4ffbf2a4671fe2098ce345488059caca08
Alecia Vogel <hello@alecia.ca>
2023-06-12 00:41:50 -0600

    Properly center the static menu links

--- src/components/site/StaticMenu.tsx
+++ src/components/site/StaticMenu.tsx
@@ -35,9 +35,9 @@ export default function StaticMenu() {
   return (
     <nav
       aria-label="Main"
-      className="static-menu absolute top-0 left-0 z-10 py-4 w-full pointer-events-none md:py-6"
+      className="static-menu absolute top-0 left-1/2 transform -translate-x-1/2 z-10 py-4 pointer-events-none md:py-6"
     >
-      <ul className="space-x-10 text-base h-10 flex justify-center items-center">
+      <ul className="space-x-10 text-base h-10 flex justify-center items-center last:mr-10">
         <NavLink to="/" srOnly={true}>
           Home
         </NavLink>
