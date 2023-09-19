a02d85c22d8ddcf60e6d35a2a391fea8d531498f
Alecia Vogel <hello@alecia.ca>
2023-09-19 11:37:09 -0600

    Remove resume playground, as it has been moved to another repo
    https://github.com/aleciavogel/resume-generator

--- src/components/features/resume/templates/ResumeWrapper.tsx
+++
@@ -1,6 +0,0 @@
-import { View } from "@react-pdf/renderer";
-import { tw } from "../constants";
-
-export default function ResumeWrapper({ children }: { children: React.ReactNode }) {
-  return <View style={tw("p-6 h-full bg-white")}>{children}</View>;
-}
