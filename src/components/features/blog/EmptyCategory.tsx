876a753a8ce95be2083a340eb7154cffb7898149
Alecia Vogel <hello@alecia.ca>
2023-09-24 12:28:35 -0600

    Scrape together a basic empty category state

--- src/components/features/blog/EmptyCategory.tsx
+++ src/components/features/blog/EmptyCategory.tsx
@@ -1,13 +1,27 @@
 import { type FC } from 'react'
+import Link from 'next/link'
+
 import AleciaIdeaSvg from '@/components/vectors/AleciaIdeaSvg'
 
 const EmptyCategory: FC = () => {
   return (
-    <div className="mx-auto text-center">
-      <div>
-        <AleciaIdeaSvg className="block h-80 w-80" />
+    <div className="grid grid-cols-6 gap-8 items-center">
+      <div className="col-span-2">
+        <AleciaIdeaSvg className="block w-full" />
       </div>
-      <p>There are no posts in this category yet.</p>
+      <div className="col-span-4">
+        <h2 className="font-serif text-6xl text-primary-600 dark:text-primary-400 mb-2">
+          No articles here yet!
+        </h2>
+        <p className="text-lg text-body">
+          Your patience is appreciated while I brainstorm some new ideas for this section. In the
+          meantime, feel free to check out my{' '}
+          <Link href="/blog" className="zigzag-link">
+            other posts
+          </Link>
+          !
+        </p>
+      </div>
     </div>
   )
 }
