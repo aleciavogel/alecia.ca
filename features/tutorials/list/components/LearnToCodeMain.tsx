63dcac693e97ad39e88fba12cae25c52eddb2520
Alecia Vogel <hello@alecia.ca>
2023-09-27 15:48:21 -0600

    Move variant

--- features/tutorials/list/components/LearnToCodeMain.tsx
+++ features/tutorials/list/components/LearnToCodeMain.tsx
@@ -8,7 +8,7 @@ const LearnToCodeMain: FC = () => {
   const tutorialKeys = Object.keys(TUTORIAL_LIST)
 
   return (
-    <PageMain variant="chevron" className="bg-gray-100">
+    <PageMain className="bg-gray-100">
       <ul className="px-16 md:px-20 grid grid-cols-6 gap-8">
         {tutorialKeys?.map((key) => (
           <TutorialItem key={`tutorial-item-${key}`} id={key} {...TUTORIAL_LIST[key]} />
