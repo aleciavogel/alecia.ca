e112f07a68db0e0c5e373990269c24f63942b1e0
Alecia Vogel <hello@alecia.ca>
2025-01-18 23:56:10 -0700

    :ambulance: hotfix(components): attempt to fix wavy text (also change overflow to hidd…

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -6,7 +6,7 @@ const { composePlugins, withNx } = require('@nx/next')
  * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
  **/
 const nextConfig = {
-  reactStrictMode: process.env['NODE_ENV'] === 'development',
+  reactStrictMode: false,
   nx: {
     // Set this to true if you would like to use SVGR
     // See: https://github.com/gregberge/svgr
