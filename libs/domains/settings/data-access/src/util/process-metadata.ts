b5067bee6ea7cb7cd898506bb843f08676851dc6
Alecia Vogel <hello@alecia.ca>
2025-01-20 21:01:18 -0700

    :ambulance: hotfix(components): use urlFor in ogimage metadata process…

--- libs/domains/settings/data-access/src/util/process-metadata.ts
+++ libs/domains/settings/data-access/src/util/process-metadata.ts
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 
 import { IS_VERCEL_PREVIEW, SITE_BASE_URL } from '@alecia/constants'
+import { urlFor } from '@alecia/sanity-util'
 import { getPlaceholderImage } from '@alecia/util'
 
 import { getSettings } from '../loaders'
@@ -34,7 +35,7 @@ export async function processMetadata({
   const { title = fallbackTitle, description, ogimage, noIndex } = metadata ?? {}
   const url = SITE_BASE_URL + slug
   const currentOgimage = settings.ogimage
-    ? { url: settings.ogimage }
+    ? { url: urlFor(settings.ogimage).width(1200).height(630).url() }
     : { url: getPlaceholderImage(1200, 630) }
 
   return {
@@ -46,7 +47,9 @@ export async function processMetadata({
       url,
       title,
       description,
-      images: ogimage ? { url: ogimage } : currentOgimage,
+      images: ogimage
+        ? { url: urlFor(ogimage).width(1200).height(630).fit('crop').url() }
+        : currentOgimage,
     },
     robots: {
       index: noIndex || IS_VERCEL_PREVIEW ? false : undefined,
