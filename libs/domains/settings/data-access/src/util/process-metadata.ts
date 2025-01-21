d63b654afcfb5ca145019ad91c0b3c80f511b92d
Alecia Vogel <hello@alecia.ca>
2025-01-20 21:16:21 -0700

    :ambulance: hotfix(components): adjust the metadataBase to include htt…

--- libs/domains/settings/data-access/src/util/process-metadata.ts
+++ libs/domains/settings/data-access/src/util/process-metadata.ts
@@ -2,7 +2,6 @@ import type { Metadata } from 'next'
 
 import { IS_VERCEL_PREVIEW, SITE_BASE_URL } from '@alecia/constants'
 import { urlFor } from '@alecia/sanity-util'
-import { getPlaceholderImage } from '@alecia/util'
 
 import { getSettings } from '../loaders'
 
@@ -25,26 +24,28 @@ export async function processMetadata({
   fallbackTitle,
 }: ProcessMetadataArgs): Promise<Metadata> {
   const settings = await getSettings()
+  const BASE_URL = `https://${SITE_BASE_URL}`
+  const DEFAULT_META = {
+    metadataBase: new URL(BASE_URL),
+  }
 
   if (!settings) {
-    return {
-      metadataBase: new URL(SITE_BASE_URL),
-    }
+    return DEFAULT_META
   }
 
   const { title = fallbackTitle, description, ogimage, noIndex } = metadata ?? {}
-  const url = SITE_BASE_URL + slug
+
   const currentOgimage = settings.ogimage
     ? { url: urlFor(settings.ogimage).width(1200).height(630).url() }
-    : { url: getPlaceholderImage(1200, 630) }
+    : undefined
 
   return {
-    metadataBase: new URL(SITE_BASE_URL),
+    ...DEFAULT_META,
     title: `${title} | ${settings.title}`,
     description,
     openGraph: {
       type: 'website',
-      url,
+      url: DEFAULT_META + (slug.includes('/') ? slug : `/${slug}`),
       title,
       description,
       images: ogimage
