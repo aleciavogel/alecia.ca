f6bb12e209f661f989a3ff3d6a06fd17e01da310
Alecia Vogel <hello@alecia.ca>
2025-01-25 16:48:49 -0700

    :sparkles: feat: replace twitter and facebook sharelinks

--- libs/domains/blog/ui/src/article-info/share-links.tsx
+++ libs/domains/blog/ui/src/article-info/share-links.tsx
@@ -4,36 +4,50 @@ import { IconName } from '@fortawesome/pro-regular-svg-icons'
 import { usePathname } from 'next/navigation'
 
 import { SITE_BASE_URL } from '@alecia/constants/routes'
+import { buildRoute } from '@alecia/util/routes'
 import { cn } from '@alecia/util/styles'
 
 import ShareLinkItem from './share-link-item'
 
-const FB_BASE = 'https://www.facebook.com/sharer/sharer.php?u='
-const TW_BASE = 'https://twitter.com/intent/tweet?url='
+const LINKEDIN_BASE = 'https://www.linkedin.com/sharing/share-offsite/?url={encodedUrl}'
+const BLUESKY_BASE = 'https://bsky.app/intent/compose?text={encodedText}'
+const EMAIL_BASE = 'mailto:?subject={encodedSubject}&body={encodedBody}'
 
-const FB_LABEL = 'Share on Facebook'
-const TW_LABEL = 'Share on X'
+const LINKEDIN_LABEL = 'Share on LinkedIn'
+const BLUESKY_LABEL = 'Share on Bluesky'
 const EMAIL_LABEL = 'Share via Email'
 
 const ShareLinks = () => {
-  // const data = await getPost()
-  // const title = data?.title ?? ''
-  const title = 'Check out this article on Alecia.ca'
+  const pathname = usePathname()
+  const subject = 'Check out this article 👩‍💻'
+  const url = `https://${SITE_BASE_URL}/blog/${pathname}`
+  const encodedText = encodeURIComponent(`Hey! You should give this a read: ${url}`)
 
   // Retrieve the current absolute url of the page for Next.js
-  const pathname = usePathname()
-  const encoded = encodeURIComponent(SITE_BASE_URL + pathname)
-  const emailBody = encodeURIComponent(
-    `Hey! Check out this article on Alecia.ca: ${SITE_BASE_URL}${pathname}`,
-  )
-  const emailSubject = encodeURIComponent(title)
+
+  const shareableLink = encodeURIComponent(url)
+  const shareablePost = encodeURIComponent(`${subject} ${url}`)
+  const shareableSubject = encodeURIComponent(subject)
 
   return (
     <div className={cn('mt-6 mb-16 space-x-6')}>
-      <ShareLinkItem href={FB_BASE + encoded} icon={'faFacebookF' as IconName} label={FB_LABEL} />
-      <ShareLinkItem href={TW_BASE + encoded} icon="faXTwitter" label={TW_LABEL} />
       <ShareLinkItem
-        href={`mailto:?subject=${emailSubject}&body=${emailBody}`}
+        href={buildRoute(LINKEDIN_BASE, {
+          encodedUrl: shareableLink,
+        })}
+        icon="faLinkedinIn"
+        label={LINKEDIN_LABEL}
+      />
+      <ShareLinkItem
+        href={buildRoute(BLUESKY_BASE, { encodedText: shareablePost })}
+        icon="faBluesky"
+        label={BLUESKY_LABEL}
+      />
+      <ShareLinkItem
+        href={buildRoute(EMAIL_BASE, {
+          encodedSubject: shareableSubject,
+          encodedBody: encodedText,
+        })}
         icon={'faEnvelope' as IconName}
         label={EMAIL_LABEL}
       />
