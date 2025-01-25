0af603d485a29e5cd2352446122d663e2a0e23ca
Alecia Vogel <hello@alecia.ca>
2025-01-24 20:20:05 -0700

    :bug: fix(components): style hover card arrow properly when it swaps sides due to collision

--- libs/shared/ui/src/ui/hover-card/arrow.tsx
+++ libs/shared/ui/src/ui/hover-card/arrow.tsx
@@ -5,26 +5,29 @@ import { forwardRef } from 'react'
 
 import { cn } from '@alecia/util/styles'
 
-const HoverCardArrow = forwardRef<HTMLDivElement, ComponentPropsWithoutRef<'div'>>((props, ref) => (
-  <div
-    ref={ref}
-    {...props}
-    className={cn(
-      'transition-none',
-      'relative',
-      'absolute h-0 w-0 -top-[22px] left-8',
-      'border-[12px] border-transparent border-solid',
-      'border-b-white dark:border-b-gray-900',
+import * as HoverCardPrimitive from '../popper/hover-card'
 
-      /** pseudo-element for the border */
-      'after:absolute after:h-0 after:w-0',
-      'after:border-[15px] after:border-transparent after:border-solid',
-      'after:border-b-primary-800 dark:after:border-b-primary-300',
-      'after:contents[""]',
-      'after:-top-[18px] after:-left-[15px]',
-      'after:z-[-1]',
-    )}
-  />
+const HoverCardArrow = forwardRef<HTMLDivElement, ComponentPropsWithoutRef<'div'>>((props, ref) => (
+  <HoverCardPrimitive.Arrow asChild>
+    <div
+      ref={ref}
+      {...props}
+      className={cn(
+        'transition-none',
+        'absolute',
+        'h-0 w-0 bg-transparent',
+        'border-[12px] border-transparent border-solid',
+        'border-t-white dark:border-t-gray-900',
+
+        /** pseudo-element for the border */
+        'after:absolute after:h-0 after:w-0 after:top-[-9px] after:-right-[15px]',
+        'after:border-[15px] after:border-transparent after:border-solid',
+        'after:border-t-primary-800 dark:after:border-t-primary-300',
+        'after:contents[""]',
+        'after:z-[-1]',
+      )}
+    />
+  </HoverCardPrimitive.Arrow>
 ))
 
 HoverCardArrow.displayName = 'HoverCardArrow'
