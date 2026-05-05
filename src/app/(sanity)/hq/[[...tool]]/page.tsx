67c6da3b3e04f3c037d86fbf748ddcb1c04b66a0
Alecia Vogel <hello@alecia.ca>
2026-05-05 17:47:52 -0600

    :bug: fix: testing

--- src/app/(sanity)/hq/[[...tool]]/page.tsx
+++ src/app/(sanity)/hq/[[...tool]]/page.tsx
@@ -1,9 +1,9 @@
 'use client'
 
-import { NextStudio } from 'next-sanity/studio'
-
-import sanityConfig from '@alecia/vendors/sanity/studio/sanity.config'
+// import { NextStudio } from 'next-sanity/studio'
+//
+// import sanityConfig from '@alecia/vendors/sanity/studio/sanity.config'
 
 export default function StudioPage() {
-  return <NextStudio config={sanityConfig} />
+  return <div>Hello</div>
 }
